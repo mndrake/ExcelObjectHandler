ae1ec30b250b21d57fa80ba437fb3a144e7af7ac
Dave Carlson <mndrake22@mac.com>
2013-03-30 14:07:42 -0500

    initial commit

---
+++ Example.fs
@@ -0,0 +1,53 @@
+﻿namespace ExcelObjectHandler
+open ExcelDna.Integration
+
+module TestRTD =
+
+// class example
+ 
+  // class we want to create an object handle for
+  type Person =
+    val FirstName:string
+    val LastName:string
+    new (firstName,lastName) = {FirstName=firstName; LastName=lastName}
+
+  [<ExcelFunction(Name="Person.create")>]
+  let createPerson (firstName, lastName) =
+    new Person(firstName, lastName)
+    |> XlCache.registerObject "Person"
+ 
+  // function that uses the object handle
+  [<ExcelFunction(Name="Person.getFirstName")>]
+  let getPersonFirstName personHandle = 
+     let p:Person = XlCache.lookupObject personHandle 
+     p.FirstName
+
+// slow function example
+ 
+  // slow running function restated to object array input
+  let myArray (p:obj[]) =
+      let (n,a) = (unbox p.[0], unbox p.[1])
+      System.Threading.Thread.Sleep 2000
+      [| 1 .. n |]
+
+  // creates object asynchronously and returns handle when done
+  [<ExcelFunction(Name="MyArray.create")>]
+  let createMyArray (n:int,a:int) =
+    let args:obj[] = [| n; a |]
+    XlCache.asyncRegisterObject(myArray,"MyArray",args)
+
+ 
+  [<ExcelFunction(Name="MyArray.getSum")>]
+  let getMyArraySum myArrayHandle =
+     let a:int[] = XlCache.lookupObject myArrayHandle
+     a |> Array.sum
+
+  let mySlowFunction (p:obj[]) =
+    let myString:string = unbox p.[0]
+    System.Threading.Thread.Sleep 2000
+    box myString
+
+  [<ExcelFunction(Name="AsyncMySlowFunction")>]
+  let asyncMySlowFunction (s:obj) =
+    let args:obj[] = [| s |]
+    XlCache.asyncRun(mySlowFunction,"MySlowFunction",args)
---
+++ ObjectHandler.fs
@@ -0,0 +1,65 @@
+﻿namespace ExcelObjectHandler
+open System.Collections.Generic
+open ExcelDna.Integration
+open ExcelDna.Integration.Rtd 
+open ExcelDna.Integration
+
+type public ExcelInterface() = 
+  interface IExcelAddIn with
+    member x.AutoOpen() = ExcelAsyncUtil.Initialize()
+    member x.AutoClose() = ExcelAsyncUtil.Uninitialize()
+
+module XlCache =
+  [<Literal>]
+  let RTDServer = "ExcelObjectHandler.StaticRTD"
+  let private _objects = new Dictionary<string,obj>()
+  let private _tags = new Dictionary<string, int>()
+  let lookup handle = 
+      match _objects.ContainsKey(handle) with
+      |true -> _objects.[handle]
+      |false -> failwith "handle not found"
+  let register tag (o:obj) = 
+      let counter = 
+          match _tags.ContainsKey(tag) with
+          |true  -> _tags.[tag] + 1
+          |false -> 1
+      _tags.[tag] <- counter
+      let handle = sprintf "[%s.%i]" tag counter
+      _objects.[handle] <- o
+      handle
+  let unregister handle =    
+      if _objects.ContainsKey(handle) then
+          _objects.Remove(handle) |> ignore
+  let inline lookupObject (handle:string):'a =
+      lookup handle |> unbox
+  let inline registerObject tag (o:obj) = 
+      o |> register tag
+        |> fun name -> XlCall.RTD(RTDServer, null, name)
+  let inline asyncRun (myFunction, tag, myInputs:obj[]) =
+      ExcelAsyncUtil.Run(tag, myInputs, fun () -> (myFunction myInputs) |> box)
+      |> unbox
+      |> fun result ->
+            if result = box ExcelError.ExcelErrorNA then 
+                box ExcelError.ExcelErrorGettingData
+            else result
+  let inline asyncRegisterObject (myFunction, tag, myInputs:obj[]) =
+      ExcelAsyncUtil.Run(tag, myInputs, fun () -> (myFunction myInputs) |> box)
+      |> unbox
+      |> fun result ->
+            if result = box ExcelError.ExcelErrorNA then 
+                box ExcelError.ExcelErrorGettingData
+            else result |> register tag
+                        |> fun name -> XlCall.RTD(RTDServer, null, name)    
+
+
+type public StaticRTD() =
+  inherit ExcelRtdServer() 
+  let _topics = new Dictionary<ExcelRtdServer.Topic, string>()
+  override x.ConnectData(topic, topicInfo, newValues) =
+          let name = topicInfo.[0]
+          _topics.[topic] <- name
+          box name
+  override x.DisconnectData(topic:ExcelRtdServer.Topic) =
+             _topics.[topic] |> XlCache.unregister
+             _topics.Remove(topic) |> ignore
+
